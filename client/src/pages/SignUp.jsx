849b8da7ec928d1fba8388217b3ac49569acf685
NGUlc <124418227+NGUlc@users.noreply.github.com>
2023-10-21 19:23:20 +0800

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -44,7 +44,7 @@ export default function SignUp() {
   return (
     <div className='max-w-lg p-3 mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
-        SignUp
+        Sign Up
       </h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type ='text' placeholder='username'
