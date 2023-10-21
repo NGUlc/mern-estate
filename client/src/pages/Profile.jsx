f34d6202cd82fc0e62c2e8812a8949c9b2c24e87
NGUlc <124418227+NGUlc@users.noreply.github.com>
2023-10-21 23:17:48 +0800

    complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import {useSelector} from 'react-redux';
 
 export default function Profile() {
+  const {currentUser} = useSelector ((state) => state.user);
   return (
-    <div>Profile</div>
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+         <img src={currentUser.avator} alt="profile"
+          className='self-center object-cover w-24 h-24 mt-2 rounded-full cursor-pointer ' /> 
+          <input type="text" placeholder='username' id='username' 
+          className='p-3 border rounded-lg' /> 
+          <input type="email" placeholder='email' id='email'
+          className='p-3 border rounded-lg' />
+          <input type="text" placeholder='password' id='password'
+          className='p-3 border rounded-lg' />
+          <button className='p-3 text-white uppercase rounded-lg bg-slate-700 hover: opacity-95'>update</button>
+      </form>
+      <div className= 'flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer '>Delete account</span> 
+        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        </div>
+    </div>
   )
 }
