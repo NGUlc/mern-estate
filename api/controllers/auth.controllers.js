f6e9ee9aef5ead6803dec855dd40bfdc67669e00
NGUlc <124418227+NGUlc@users.noreply.github.com>
2023-10-21 08:22:42 +0800

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js'
-import { errorHandler } from '../utils/error.js';
+
 export const signup = async(req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
