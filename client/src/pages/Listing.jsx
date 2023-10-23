b5b70a93bf15405bc96a41394ca0559e089b96d4
NGUlc <124418227+NGUlc@users.noreply.github.com>
2023-10-23 23:39:46 +0800

    add contact landlord functionality to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,9 +2,11 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import {useSelector} from 'react-redux';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { FaBed, FaBath, FaMapMarkerAlt, FaShare, FaParking, FaChair } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -12,7 +14,10 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
+    
     const params = useParams();
+    const {currentUser} = useSelector((state) => state.user);
     useEffect(() => {
         const fetchListing = async () => {
             try {
@@ -35,7 +40,6 @@ export default function Listing() {
         };
         fetchListing();
     }, [params.listingId])
-    console.log(loading)
   return (
     <main>
         {loading && <p className='text-2xl text-center my-7'>Loading...</p>}
@@ -114,6 +118,10 @@ export default function Listing() {
                         {listing.furnished ? 'Furnished' : 'Unfurnished'}
                     </li>
                 </ul>
+                {currentUser && listing.userRef !== currentUser._id && !contact && (
+                    <button onClick={() => setContact(true)} className='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95'>Contact landlord</button>
+                )} 
+                {contact && <Contact listing = {listing}/>}   
             </div>   
              
         </div>
