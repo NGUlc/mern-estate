bc3d03b479d2986c5ce47f24e731a1113193e719
NGUlc <124418227+NGUlc@users.noreply.github.com>
2023-10-24 11:22:38 +0800

    create search api rouutes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,4 +8,5 @@ router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
 router.get('/:id', verifyToken, getUser);
+
 export default router;
